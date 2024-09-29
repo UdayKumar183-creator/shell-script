0e7817f84a892d2b96d7470ce13931db6ad29647
UdayKumar183-creator <javvajiudaykumar59@gmail.com>
2024-09-29 23:41:40 +0530

    added 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh: Hello Suresh, Good Mrning"
-echo "Suresh: Hi Ramesh, VGM"
-echo "Ramesh: How r u Suresh"
-echo "Suresh: I am good Ramesh, HRU"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2, Good Mrning"
+echo "$PERSON: Hi $PERSON1, VGM"
+echo "$PERSON1: How r u $PERSON2"
+echo "$PERSON2: I am good $PERSON1, HRU"
