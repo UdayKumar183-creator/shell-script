56cc052be2bce0a214d2fc9e5b2f9b3eb93a5aec
UdayKumar183-creator <javvajiudaykumar59@gmail.com>
2024-10-06 00:40:18 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script name: $0"
 
@@ -22,11 +25,11 @@ else
     echo "You are root user"
 fi # fi means reverse of if
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? "Installing MySQL"
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 VALIDATE $? "Installing GIT"
 
