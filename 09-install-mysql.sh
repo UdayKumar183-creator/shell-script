6f5cde2a7ae7472036e094501070950a69684eeb
UdayKumar183-creator <javvajiudaykumar59@gmail.com>
2024-10-05 18:30:41 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -19,3 +19,15 @@ then
 else
     echo "Installing MySQL is success"
 fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
+else
+    echo "Installing GIT is success"
+fi
+
+
